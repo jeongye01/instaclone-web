76922ef2919d84240e5093f3de6de2d5ceac09cc
jeongye01 <74299317+jeongye01@users.noreply.github.com>
2021-12-01 18:28:34 +0900

    likes

--- instaclone-web/src/screens/Home.js
+++ instaclone-web/src/screens/Home.js
@@ -1,11 +1,37 @@
 import React from "react";
 import {  logUserOut } from '../apollo';
+import { gql, useQuery } from "@apollo/client";
+
+
+
+const FEED_QUERY=gql`
+  query seeFeed{
+    seeFeed{
+      id
+      user{
+        username
+        avatar
+      }
+      file
+      caption
+      likes
+      comments
+      createdAt
+      isMine
+      isLiked
+    }
+  }
+`;
+
+
 const Home=()=>{
-  
+  const { data } = useQuery(FEED_QUERY);
   return (
     <div>
-      <h1>Home</h1>
-      <button onClick={()=>logUserOut()}>Log out now!</button>
+      {data?.seeFeed?.map((photo) => (
+      
+        <Photo key={photo.id} {...photo}/>   
+      ))}
     </div>
   );
 };
